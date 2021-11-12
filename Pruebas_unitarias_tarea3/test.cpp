dd1c78d7a6c4bba90ee1e749a748891fb004311b
diegomonroe <diego-monroe@hotmail.com>
2021-11-12 13:43:11 -0600

    Se agrega test personaNoValida

--- Pruebas_unitarias_tarea3/test.cpp
+++ Pruebas_unitarias_tarea3/test.cpp
@@ -1,50 +1,62 @@
 #include "pch.h"
 #include <string>
-/*
+
 #include "../Tarea_3/escritorLibros.h"
+#include "../Tarea_3/escritorLibros.cpp"
 #include "../Tarea_3/lectorLibros.h"
+#include "../Tarea_3/lectorLibros.cpp"
 #include "../Tarea_3/libro.h"
+#include "../Tarea_3/libro.cpp"
+#include "../Tarea_3/lector.h"
+#include "../Tarea_3/lector.cpp"
+#include "../Tarea_3/excepcionLibroNoExiste.h"
+#include "../Tarea_3/excepcionPersonaNoValida.h"
 
 namespace {
     TEST(EscritorTest, Prueba_EscribirUnLibro) {
 
-        Libro libroPrueba{ 15, "Prueba", "12345" };
+        Libro libroPrueba{ 15, "nombrePrueba","apellidoPrueba", "12345"};
 
-        EscritorLibros escritor{ "prueba.dat" };
-        escritor.AgregarLibro(libroPrueba)
-            escritor.Cerrar();
+        EscritorLibros escritor{"prueba.dat"};
+        escritor.agregarLibro(libroPrueba);
+        escritor.cerrar();
 
         // Leer el libro de prueba
         LectorLibros lector{ "prueba.dat" };
-        libroLeido = lector.ObtenerLibro(0);
-        lector.Cerrar()
+        Libro libroLeido = lector.ObtenerLibro(1);
+        lector.cerrar();
 
-            EXPECT_EQ(libroLeido.ID, libroPrueba.ID);
-        EXPECT_EQ(libroLeido.Nombre, libroPrueba.Nombre);
-        EXPECT_EQ(libroLeido.ISBN, libroPrueba.ISBN);
+        EXPECT_EQ(libroLeido.getID(), libroPrueba.getID());
+        EXPECT_EQ(libroLeido.getNombre(), libroPrueba.getNombre());
+        EXPECT_EQ(libroLeido.getApellido(), libroPrueba.getApellido());
+        EXPECT_EQ(libroLeido.getCorreo(), libroPrueba.getCorreo());
     }
 
     TEST(EscritorTest, Prueba_ExcepcionLecturaLibroNoExiste) {
+        Libro libroPrueba{ 15, "nombrePrueba","apellidoPrueba", "correoPrueba@prueba.com"};
+        EscritorLibros escritor{ "prueba.dat" };
+        escritor.agregarLibro(libroPrueba);
+        escritor.cerrar();
+
+        // Leer el libro de prueba
+        LectorLibros lector{"prueba.dat"};
         EXPECT_THROW({
-
-            Libro libroPrueba { 15, "Prueba", "12345" };
-
-            EscritorLibros escritor {"prueba.dat"};
-            escritor.AgregarLibro(libroPrueba)
-            escritor.Cerrar();
-
-            // Leer el libro de prueba
-            LectorLibros lector {"prueba.dat"};
-            libroLeido = lector.ObtenerLibro(5);
-            lector.Cerrar();
-
+            Libro libroLeido = lector.ObtenerLibro(5);
+            lector.cerrar();
             }, ExcepcionLibroNoExiste);
-
     }
 
+    TEST(LectorTest, Prueba_ExcepcionPersonaNoValida) {
+        Libro libroPrueba{ 0, "nombrePrueba","apellidoPrueba", "correoPrueba@prueba.com" };
+        EscritorLibros escritor{"prueba.dat"};
+        escritor.agregarLibro(libroPrueba);
+        escritor.cerrar();
+
+        // Leer el libro de prueba
+        LectorLibros lector{ "prueba.dat" };
+        EXPECT_THROW({
+            Libro libroLeido = lector.ObtenerLibro(1);
+            lector.cerrar();
+            }, ExcepcionPersonaNoValida);
+    }
 }
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
-*/
